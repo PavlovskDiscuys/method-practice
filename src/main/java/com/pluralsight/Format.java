bf17313a3f946b957b5e424f5eda408f1bbc4e26
Pav <robsarmiento@icloud.com>
2026-04-13 15:40:59 -0500

    Format has been completed

--- src/main/java/com/pluralsight/Format.java
+++ src/main/java/com/pluralsight/Format.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
+import  java.util.Scanner;
 
 public class Format {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your first name: ");
+        String first = input.nextLine();
+
+        System.out.println("What is  your last name: ");
+        String last = input.nextLine();
+
+        System.out.println(formatName(first,last));
+    }
+
+    public static String formatName(String first, String last) {
+        return last + ", " + first;
+    }
+
+
+
 }
