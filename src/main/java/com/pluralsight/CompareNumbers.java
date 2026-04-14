25325b7bacb551147593bd67fdee8f92a27549cd
Pav <robsarmiento@icloud.com>
2026-04-14 09:31:39 -0500

    CompareNumbers has been completed
    Update Log:
    -Added measure of is number even
    -Added measure is number positive

--- src/main/java/com/pluralsight/CompareNumbers.java
+++ src/main/java/com/pluralsight/CompareNumbers.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class CompareNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter a Number: ");
+        int firstInput = input.nextInt();
+
+        System.out.println("Is the number even? ");
+        System.out.println(isEven(firstInput));
+
+        System.out.println("Is the number positive ");
+        System.out.println(isPositive(firstInput));
+    }
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static boolean isPositive(double number) {
+        return number >- 0;
+    }
+
 }
